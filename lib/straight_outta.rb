66ff9874e87691049df31c8a423c79ff4ed28544
Matt Moore <mattmoore@carbonhelix.com>
2016-10-24 15:37:34 -0500

    Improved algorithm for arranging image elements. Automatically adjust text pointsize to fit within the frame.

--- lib/straight_outta.rb
+++ lib/straight_outta.rb
@@ -3,6 +3,8 @@ require "rmagick"
 
 module StraightOutta
   def self.somewhere(location, file_path)
+    location = location.upcase
+
     spec = Gem::Specification.find_by_name('straight_outta')
     resource_path = "#{spec.gem_dir}/resources"
 
@@ -10,24 +12,39 @@ module StraightOutta
     frame = Magick::Image.read("#{resource_path}/images/logo-frame-2x.png").first
     grime = Magick::Image.read("#{resource_path}/images/pattern-grime.jpg").first
 
-    x = (background.columns / 2) - (frame.columns / 2)
-    y = (background.rows / 2) - (frame.rows / 2)
+    crop_x = (background.columns / 2) - (frame.columns / 2)
+    crop_y = (background.rows / 2) - (frame.rows / 2)
+
+    # Crop background to the frame
+    background.crop!(crop_x, crop_y, frame.columns, frame.rows)
 
     # Composite background and frame
-    result = background.composite!(frame, x, y, Magick::OverCompositeOp)
+    result = background.composite(frame, 0, 0, Magick::OverCompositeOp)
 
-    # Text with grime
-    text = Magick::Draw.new
-    text.annotate(result, 768, 824, x, 406, location.upcase) do
-      self.gravity = Magick::CenterGravity
-      self.pointsize = 255
-      self.font = "#{resource_path}/fonts/champion-htf-featherweight.ttf"
-      self.font_weight = Magick::NormalWeight
-      self.tile = grime
+    # User-specified location text
+    gc = Magick::Draw.new
+    gc.gravity = Magick::CenterGravity
+    gc.font = "#{resource_path}/fonts/champion-htf-featherweight.ttf"
+    gc.font_weight = Magick::NormalWeight
+    pointsize = 250
+    gc.pointsize = pointsize
+
+    # Calculate pointsize down if text width is wider than the frame
+    metrics = gc.get_type_metrics(location)
+    while metrics[:width] > result.columns - 20
+      pointsize = pointsize - 1
+      gc.pointsize = pointsize
+      metrics = gc.get_type_metrics(location)
     end
 
-    result.crop!(x, y, 768, 824)
+    gc.tile = grime
+    gc_width = 0
+    gc_height = 0
+    gc_pos_x = 0
+    gc_pos_y = 318
+    gc.annotate(result, gc_width, gc_height, gc_pos_x, gc_pos_y, location)
 
+    result.format = 'png'
     result.write(file_path)
   end
 end
